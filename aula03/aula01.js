8a734a1923bdc8b7c6855b792d1d176057bec0d8
Victor Reis <victor.h.s.reis@gmail.com>
2020-01-05 20:16:06 -0300

    Notes apps

--- aula03/aula01.js
+++ aula03/aula01.js
@@ -1,6 +1,44 @@
 const getNotes = require('./notes')
 const chalk = require('chalk')
-const notes = getNotes()
+const yargs = require('yargs')
 
-console.log(notes)
-console.log(chalk.black.bold.inverse.bgGreenBright("Success!"))
+// custumize version
+yargs.version('1.25')
+
+// Add, remove, read, list
+// Create add command
+
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    handler: function (){
+        console.log(chalk.bgGray("Add new notes"))
+    }
+})
+//Create a remove command
+
+yargs.command({
+    command:'remove',
+    describe: 'Remove a note',
+    handler: function (){
+        console.log(chalk.red('Removing a note'))
+    }
+})
+
+//Create a list command
+yargs.command({
+    command:'list',
+    describe: 'List all notes',
+    handler: function(){
+        console.log('This is all yours notes')
+    }
+})
+//Create a read command
+yargs.command({
+    command:'read',
+    describe:' Read a note',
+    handler: function(){
+        console.log('Read a note ...')
+    }
+})
+console.log(yargs.argv)
