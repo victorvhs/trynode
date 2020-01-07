b6791fad65fc2190bfcbcaa090b271a1929820f2
Victor Reis <victor.h.s.reis@gmail.com>
2020-01-06 21:37:48 -0300

    filter api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,11 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/b030f91cc86d167e7740ee92f1575fee/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/b030f91cc86d167e7740ee92f1575fee/37.8267,-122.4233?lang=pt&units=si'
 
-request({url:url}, (error,response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({url:url,json:true}, (error,response) => {
+    //console.log(response.body.currently)
+    const currently =  response.body.currently
+    const daily = response.body.daily
+    console.log(daily.data[0].summary +' It is currently '+ currently.temperature +' degrees out. There is a '+ currently.precipProbability +'% chance of rain')
 
 })
