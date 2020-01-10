cf193668b10c236628fe0d841c83fcceaa35a7f5
Victor Reis <victor.h.s.reis@gmail.com>
2020-01-10 19:43:22 -0300

    fetch data from the server

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,12 +6,21 @@ console.log("Javasctrip is here")
 //     })
 // })
 
-const urlData ='http://localhost:3000/weather?address=!' 
-fetch(urlData).then((response) =>{
-    response.json().then((data)=>{
-        if(data.error){
-            return console.error("Erro: ",data.error)
-        }
-        console.log(data)
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    const location = search.value
+
+    const urlData ='http://localhost:3000/weather?address='+location 
+    fetch(urlData).then((response) =>{
+        response.json().then((data)=>{
+            if(data.error){
+                return console.error("Erro: ",data.error)
+            }
+            console.log(data)
+        })
     })
+
 })
