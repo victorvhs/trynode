bffdc993ad1de1883e831af97af658346f59238b
Victor Reis <victor.h.s.reis@gmail.com>
2020-01-14 20:37:50 -0300

    mongo02

--- taskApp/mongodb.js
+++ taskApp/mongodb.js
@@ -1,19 +1,25 @@
 // * CRUD mongodb
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+
+const {MongoClient,ObjectID} = require('mongodb')
 
 const connectionURL ='mongodb://127.0.0.1:27017'
 const databaseName = 'task-app'
 
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
 MongoClient.connect(connectionURL, {useNewUrlParser:true,useUnifiedTopology: true }, (error, client) =>{
     if(error){
         return console.log('Error: '+error)
     }
     const db = client.db(databaseName)
     // db.collection('users').insertOne({
-    //     name: 'Reis',
-    //     age: 28
+    //     _id:id,
+    //     name: 'Santos',
+    //     age: 21
     // },(error,result) =>{
     //     if(error) return console.log("error")
     //      console.log(result.ops)
@@ -34,25 +40,31 @@ MongoClient.connect(connectionURL, {useNewUrlParser:true,useUnifiedTopology: tru
 
     // })
 
-    db.collection('task').insertMany([
-        {
-            description: 'Read one chapter',
-            completed: true,
-            date: Date.now()
-        },{
-            description: 'Eat the salad',
-            completed: false,
-            date: Date.now()
-        },{
-            description: 'Study languages',
-            completed: true,
-            date: Date.now()
-        }
-    ],
-        (error, result)=>{
-            if (error) return console.log('error')
-            console.log(result.ops)
-        })
+    // db.collection('task').insertMany([
+    //     {
+    //         description: 'Read one chapter',
+    //         completed: true,
+    //         date: Date.now()
+    //     },{
+    //         description: 'Eat the salad',
+    //         completed: false,
+    //         date: Date.now()
+    //     },{
+    //         description: 'Study languages',
+    //         completed: true,
+    //         date: Date.now()
+    //     }
+    // ],
+    //     (error, result)=>{
+    //         if (error) return console.log('error')
+    //         console.log(result.ops)
+    //     })
 
+
+    db.collection('users').findOne({name:'GEM'}, (error,user) =>{
+        if(error) return console.log("erro: "+ error)
+        console.log(user)
+
+    })
 
 })
