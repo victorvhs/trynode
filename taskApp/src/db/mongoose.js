2e1497320bf95c51b3ebb1b0f960307da6eb2a73
Victor Reis <victor.h.s.reis@gmail.com>
2020-01-18 17:14:56 -0300

    endPoint usres

--- taskApp/src/db/mongoose.js
+++ taskApp/src/db/mongoose.js
@@ -4,5 +4,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-api',{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology: true
-
 })
--- taskApp/src/index.js
+++ taskApp/src/index.js
@@ -11,17 +11,35 @@ app.use(express.json())
 app.post('/users',(req,res) =>{
     const user = new User(req.body)
     user.save().then(()=>{
-        res.send(user)
+        res.status(201).send(user)
     }).catch((error)=>{
         res.status(400).send(error)
     })
 
 })
 
+app.get('/users', (req,res)=>{
+    User.find({}).then((users)=>{
+        res.send(users)
+    }).catch((error)=>{
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id',(req,res)=>{
+    User.findById(req.params.id).then((user)=>{
+        if(!user) return res.status(404).send()
+        res.send(user)
+    }).catch((error)=>{
+        res.status(500).send()
+
+    })
+})
+
 app.post('/tasks',(req,res)=>{
     const task = new Task(req.body)
     task.save().then(()=>{
-        res.send(task)
+        res.status(201).send(task)
     }).catch((error)=>{
         res.status(400).send(error)
     })
