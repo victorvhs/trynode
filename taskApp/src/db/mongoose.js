4fae4107eb01bd7269aff673653973937da5ef37
Victor Reis <victor.h.s.reis@gmail.com>
2020-01-18 16:53:32 -0300

    endPoint for task

--- taskApp/src/db/mongoose.js
+++ taskApp/src/db/mongoose.js
@@ -6,19 +6,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-api',{
     useUnifiedTopology: true
 
 })
-
-const Task = mongoose.model('Task',{
-    description:{
-        type:String,
-        required: true,
-        trim:true
-    },
-    completed:{ 
-        type:Boolean,
-        default: false
-    },
-    date:{
-        type:Date,
-        default: Date.now
-    }
-})
