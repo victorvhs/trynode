08a2484ebcb9815165401810dbf3081fa32cf9b1
Victor Reis <victor.h.s.reis@gmail.com>
2020-01-18 17:29:17 -0300

    endPoint Task

--- taskApp/src/index.js
+++ taskApp/src/index.js
@@ -36,6 +36,24 @@ app.get('/users/:id',(req,res)=>{
     })
 })
 
+app.get('/tasks',(req,res)=>{
+    Task.find({}).then((tasks)=>{
+        res.send(tasks)
+    }).catch((error)=>{
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id',(req,res)=>{
+    const _id = req.params.id
+    Task.findById(_id).then((task)=>{
+        if(!task) return res.status(404).send()
+        res.send(task)
+    }).catch((error)=>{
+        res.status(510).send()
+    })
+})
+
 app.post('/tasks',(req,res)=>{
     const task = new Task(req.body)
     task.save().then(()=>{
