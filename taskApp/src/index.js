0e1f84144fbd2f3067627a091e7d7a595708882c
Victor Reis <victor.h.s.reis@gmail.com>
2020-01-20 18:45:11 -0300

    update tasks

--- taskApp/src/index.js
+++ taskApp/src/index.js
@@ -49,10 +49,27 @@ app.patch('/users/:id', async (req,res)=>{
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true, runValidators:true})
+        const user = await User.findByIdAndUpdate(req.params.id,req.body,
+            {new:true, runValidators:true})
         if(!user) return res.status(404).send()
         res.send(user)
     } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.patch('/tasks/:id', async (req,res) =>{
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['completed','description']
+    const isValidOperation = updates.every((update) =>allowedUpdates.includes(update))
+    if(!isValidOperation) return res.status(400).send({'error':'Invalid Updates'})
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id,req.body,
+            {new:true, runValidators:true})
+        if(!task) return res.status(404).send()
+        res.send(task)
+    } catch (e) {
+        console.log("e"+e)
         res.status(400).send(e)
     }
 })
