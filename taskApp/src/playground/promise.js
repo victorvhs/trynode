ba89fb95026b27fad65ba4d19d5934c580ef6e90
Victor Reis <victor.h.s.reis@gmail.com>
2020-01-19 21:53:54 -0300

    async

---
+++ taskApp/src/playground/promise.js
@@ -0,0 +1,14 @@
+require('../db/mongoose')
+const Task = require('../models/task')
+
+const deleteTaskAndCount = async (_id) => {
+    const deleteTask = await Task.findByIdAndDelete(_id)
+    const count = await Task.countDocuments({completed:false})
+    return count
+}
+deleteTaskAndCount('5e23120c0d03e908f8f86a').then((count) =>{
+    console.log(count)
+    process.exit()
+}).catch((e =>{
+    console.log(e)
+}))
