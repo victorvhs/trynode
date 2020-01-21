073302d028859ae3f9433e7d3ad254d328c888fa
Victor Reis <victor.h.s.reis@gmail.com>
2020-01-20 22:16:52 -0300

    Middleware

--- taskApp/src/router/user.js
+++ taskApp/src/router/user.js
@@ -42,8 +42,10 @@ router.patch('/users/:id', async (req,res)=>{
     if (!isValidOperation)return res.status(400).send({'error':'Invalid Updates'})
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id,req.body,
-            {new:true, runValidators:true})
+        const user = await User.findById(req.params.id)
+        updates.forEach((update)=> user[update] = req.body[update])
+        await user.save()
+        //const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true, runValidators:true})
         if(!user) return res.status(404).send()
         res.send(user)
     } catch (e) {
