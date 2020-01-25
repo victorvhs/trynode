153e243ed66b6ae4983c095f690d61e395a2c721
Victor Reis <victor.h.s.reis@gmail.com>
2020-01-25 15:01:55 -0300

    tasks auth

--- taskApp/src/router/task.js
+++ taskApp/src/router/task.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Task = require('../models/task')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 
@@ -33,20 +34,22 @@ router.patch('/tasks/:id', async (req,res) =>{
     }
 })
 
-router.get('/tasks', async (req,res)=>{
+router.get('/tasks',auth ,async (req,res)=>{
 
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+        //const tasks = await Task.findOne({owner:req.user._id})
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()        
     }
 })
 
-router.get('/tasks/:id',async (req,res)=>{
+router.get('/tasks/:id',auth ,async (req,res)=>{
     const _id = req.params.id
     try {
-        const task = await Task.findById(_id)
+        //const task = await Task.findById(_id)
+        const task = await Task.findOne({_id, owner:req.user._id})
         if (!task) return res.status(404).send()
         res.send(task)
     } catch (e) {
@@ -54,8 +57,12 @@ router.get('/tasks/:id',async (req,res)=>{
     }
 })
 
-router.post('/tasks', async (req,res)=>{
-    const task = new Task(req.body)
+router.post('/tasks', auth,async (req,res)=>{
+    
+    const task = new Task({
+        ...req.body,
+        owner:req.user._id
+    })
     try {
         await task.save(task)
         res.status(201).send(task)
--- taskApp/src/router/user.js
+++ taskApp/src/router/user.js
@@ -61,11 +61,8 @@ router.patch('/users/me', auth, async (req,res)=>{
     if (!isValidOperation)return res.status(400).send({'error':'Invalid Updates'})
 
     try {
-        const user = await User.findById(req.params.id)
         updates.forEach((update)=> req.user[update] = req.body[update])
         await req.user.save()
-        //const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true, runValidators:true})
-        // if(!user) return res.status(404).send()
         res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
