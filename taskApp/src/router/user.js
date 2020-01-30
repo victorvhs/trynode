a071275e6360a4d136bed2f2b279041431084d94
Victor Reis <victor.h.s.reis@gmail.com>
2020-01-29 21:08:41 -0300

    upload

--- taskApp/src/router/user.js
+++ taskApp/src/router/user.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const multer = require('multer')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
+const upload = multer({
+    dest:'upload/avatar'
+})
 
 router.post('/users', async (req,res) =>{
     const user = new User(req.body)
@@ -39,6 +43,7 @@ router.post('/users/logout', auth, async (req,res) =>{
         
     }
 })
+
 router.post('/users/logoutAll', auth, async (req,res) =>{
     try {
         req.user.tokens = []
@@ -78,6 +83,11 @@ router.delete('/users/me', auth, async (req,res)=>{
     } catch (e) {
         res.status(500).send()        
     }
+})
+
+router.post('/users/me/avatar', upload.single('avatar') , async (req,res) =>{
+    res.send()
+
 })
 
 module.exports = router
